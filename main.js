62aad2dd8433f6899e58c41676301ff418742393
mro <168573711+Marula3021@users.noreply.github.com>
2025-10-24 11:32:23 +0200

    Camputer choice function
    Add function for the computer choice that randomly
    chose between paperock and scissors
    
    Remove console.log test from html

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-console.log('Hello World')
-console.log(1+4)
+console.log("Hello World");
+console.log(1 + 4);
+
+function getComputerChoice() {
+  const choiceOne = "rock";
+  const choiceTwo = "paper";
+  const choiceThree = "scissors";
+
+  const randomChoice = Math.floor(Math.random() * 3) + 1;
+
+  let computerResult = "";
+
+  if (randomChoice === 1) {
+    computerResult = choiceOne;
+  } else if (randomChoice === 2) {
+    computerResult = choiceTwo;
+  } else {
+    computerResult = choiceThree;
+  }
+  return computerResult.toLowerCase();
+}
+
+console.log(getComputerChoice())
