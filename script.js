066f968eaf22cb074f57e723b3f6dd112f4f1741
mro <168573711+Marula3021@users.noreply.github.com>
2026-03-12 12:35:46 +0100

    improve getComputerChoice function: simplify getComputerChoice function by using an array and Math.random to select a random choice for the computer.

--- script.js
+++ script.js
@@ -2,22 +2,8 @@
 // console.log(1 + 4);
 
 function getComputerChoice() {
-  const choiceOne = "rock";
-  const choiceTwo = "paper";
-  const choiceThree = "scissors";
-
-  const randomChoice = Math.floor(Math.random() * 3) + 1;
-
-  let computerResult = "";
-
-  if (randomChoice === 1) {
-    computerResult = choiceOne;
-  } else if (randomChoice === 2) {
-    computerResult = choiceTwo;
-  } else {
-    computerResult = choiceThree;
-  }
-  return computerResult.toLowerCase();
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
 // console.log(getComputerChoice());
